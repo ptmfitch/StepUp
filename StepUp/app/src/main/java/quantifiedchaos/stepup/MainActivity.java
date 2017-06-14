271a40a6351d13fb9b09c2634d5708c008b4f1fa
Ethan <EthanChung88@gmail.com>
2017-06-14 15:53:59 +0100

    bug fix or not clearing old alarm

--- StepUp/app/src/main/java/quantifiedchaos/stepup/MainActivity.java
+++ StepUp/app/src/main/java/quantifiedchaos/stepup/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
+import android.app.TimePickerDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -26,12 +27,16 @@ import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends Activity{
+import java.util.Calendar;
+
+public class MainActivity extends Activity implements View.OnClickListener {
     private FloatingActionButton mSettingsButton;
     private SensorManager mSensorManager;
     private Sensor mSensor;
@@ -39,8 +44,14 @@ public class MainActivity extends Activity{
     static TextView TimeText;
     static TextView userTime;
     static TextView IntervalTime;
-    static CountDownTimer mTimer;
+    CountDownTimer mTimer;
     static int mIntervalTime = 30000;
+
+    static Button btnTimePickerStart, btnTimePickerEnd;
+    static TextView txtTimeStart, txtTimeEnd;
+    static private int mHourStart, mMinuteStart, mHourEnd, mMinuteEnd;
+
+    static boolean start = false, end = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,36 +65,29 @@ public class MainActivity extends Activity{
         userTime = findViewById(R.id.TimeInterval);
         IntervalTime = findViewById(R.id.SetInterval);
 
+        btnTimePickerStart = findViewById(R.id.StartTime);
+        btnTimePickerEnd = findViewById(R.id.EndTime);
+        txtTimeStart = findViewById(R.id.StartTimeText);
+        txtTimeEnd = findViewById(R.id.EndTimeText);
+
+        btnTimePickerStart.setOnClickListener(this);
+        btnTimePickerEnd.setOnClickListener(this);
+
+        btnTimePickerStart.setOnClickListener(this);
+        btnTimePickerEnd.setOnClickListener(this);
 
         mSettingsButton = findViewById(R.id.settingsButton);
-        mSettingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-
-                if(!userTime.getText().toString().isEmpty()){
-
-
-                    mIntervalTime = 60*(1000 * Integer.parseInt(userTime.getText().toString()));
-                    IntervalTime.setText("Minutes Interval: " + (mIntervalTime / 1000)/60);
-                    mTimer.cancel();
-                    startTimer(mIntervalTime);
-                    mTimer.start();
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "Fill in all boxes!", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
+        mSettingsButton.setOnClickListener(this);
 
         TimeText = findViewById(R.id.TimeText);
         startTimer(mIntervalTime);
-
-
-
     }
 
     private void startTimer(int secondsLength){
+            if(mTimer !=null){
+                mTimer.cancel();
+                mTimer = null;
+            }
             mTimer = new CountDownTimer(secondsLength, 1000) {
 
             public void onTick(long millisUntilFinished) {
@@ -91,8 +95,24 @@ public class MainActivity extends Activity{
             }
 
             public void onFinish() {
-                TimeText.setText("done!");
-                triggerAlarm();
+                Calendar rightNow = Calendar.getInstance();
+                int currentHour = rightNow.get(Calendar.HOUR_OF_DAY); // return the hour in 24 hrs format (ranging from 0-23)
+                int currentMinute = rightNow.get(Calendar.MINUTE); // return the minute
+                // TODO: 14/06/17 CURRENTLY only support positive time eg. 1pm to 3 pm not over 24 hours eg. 3pm to  1pm next day
+                // TODO: 14/06/17 Using  0:00 for both times just freezes program.
+                if((currentHour > mHourStart && currentHour < mHourEnd) ){
+                    TimeText.setText("done!");
+                    triggerAlarm();
+                }
+                else if(currentHour == mHourStart || currentHour == mHourEnd){
+                    if((currentMinute > mMinuteStart) && (currentMinute < mMinuteEnd)){
+                        TimeText.setText("done!");
+                        triggerAlarm();
+                    }
+                else{
+                        TimeText.setText("Tracker Paused!");
+                    }
+                }
             }
         };
     }
@@ -135,6 +155,70 @@ public class MainActivity extends Activity{
     }
 
 
+    @Override
+    public void onClick(View v){
 
+        if (v == mSettingsButton){
+            if(!userTime.getText().toString().isEmpty() && (start == true && end == true)){
+                mIntervalTime = 60*(1000 * Integer.parseInt(userTime.getText().toString()));
+                IntervalTime.setText("Minutes Interval: " + (mIntervalTime / 1000)/60);
+                startTimer(mIntervalTime);
+                mTimer.start();
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Fill in all boxes!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (v == btnTimePickerStart) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHourStart = c.get(Calendar.HOUR_OF_DAY);
+            mMinuteStart = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            txtTimeStart.setText(hourOfDay + ":" + minute);
+                            mHourStart = hourOfDay;
+                            mMinuteStart = minute;
+                        }
+                    }, mHourStart, mMinuteStart, false);
+            timePickerDialog.show();
+            start = true;
+
+        }
+
+        if (v == btnTimePickerEnd) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHourEnd = c.get(Calendar.HOUR_OF_DAY);
+            mMinuteEnd = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            txtTimeEnd.setText(hourOfDay + ":" + minute);
+                            mHourEnd = hourOfDay;
+                            mMinuteEnd = minute;
+                        }
+                    }, mHourEnd, mMinuteEnd, false);
+            timePickerDialog.show();
+            end = true;
+        }
+    }
 }
+
 

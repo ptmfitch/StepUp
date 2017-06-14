422daef818479b049cee9f7473247b942f2c18e9
Ethan <EthanChung88@gmail.com>
2017-06-14 12:14:01 +0100

    Functional app with interval and step tracker
    Missing time frame check and date checks

--- StepUp/app/src/main/java/quantifiedchaos/stepup/MainActivity.java
+++ StepUp/app/src/main/java/quantifiedchaos/stepup/MainActivity.java
@@ -1,13 +1,157 @@
 package quantifiedchaos.stepup;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.os.PowerManager;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends Activity{
+    private FloatingActionButton mSettingsButton;
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    static TextView MovementText;
+    static TextView TimeText;
+    static TextView userTime;
+    static TextView IntervalTime;
+    static CountDownTimer mTimer;
+    static int mIntervalTime = 30000;
+//    static String mStartTime = "0:00 PM";
+//    static TextView StartTime;
+//    static String mEndTime = "0:00 PM";
+//    static TextView EndTime;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        MovementText = findViewById(R.id.MovementBool);
+        userTime = findViewById(R.id.TimeInterval);
+        IntervalTime = findViewById(R.id.SetInterval);
+//        EndTime = findViewById(R.id.startTime);
+//        StartTime = findViewById(R.id.endTime);
+
+        mSettingsButton = findViewById(R.id.settingsButton);
+        mSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+//                Intent intent = new Intent(MainActivity.this, SetActivity.class );
+//                startActivity(intent);
+                if(!userTime.getText().toString().isEmpty()){
+//                    mStartTime = StartTime.getText().toString();
+//                    mEndTime = EndTime.getText().toString();
+//                    StartTime.setText(mStartTime);
+//                    EndTime.setText(mEndTime);
+
+                    mIntervalTime = 60*(1000 * Integer.parseInt(userTime.getText().toString()));
+                    IntervalTime.setText("Minutes Interval: " + (mIntervalTime / 1000)/60);
+                    mTimer.cancel();
+                    startTimer(mIntervalTime);
+                    mTimer.start();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Fill in all boxes!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        TimeText = findViewById(R.id.TimeText);
+        startTimer(mIntervalTime);
+//        mTimer.cancel();
+//        mTimer.start();
+//        IntervalTime.setText("Minutes Interval: " + (mIntervalTime / 1000)/60);
+//        StartTime.setText("0:00 PM");
+//        EndTime.setText("0:00 PM");
+
+
+
     }
+
+    private void startTimer(int secondsLength){
+            mTimer = new CountDownTimer(secondsLength, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                TimeText.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                TimeText.setText("done!");
+
+                triggerAlarm();
+            }
+        };
+    }
+
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mSensorEventListener, mSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+//        mSensorManager.unregisterListener(mSensorEventListener);
+    }
+
+
+    private SensorEventListener mSensorEventListener = new SensorEventListener() {
+        private int mStep;
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.values[0] == 1.0f) {
+                mStep++;
+                mTimer.start();
+            }
+            MovementText.setText(Integer.toString(mStep));
+
+        }
+    };
+
+
+    public void triggerAlarm() {
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        r.play();
+    }
+
+
+
 }
+
